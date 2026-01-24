6176be7e3823aff14f1116f7d872fd77b41afc9b
hfaso <liujunli137@gmail.com>
2026-01-24 13:58:24 +0800

    更新案例

--- src/components/demo/LoaderModel.tsx
+++ src/components/demo/LoaderModel.tsx
@@ -1,48 +1,52 @@
-import React, { useEffect, useRef, useState } from 'react';
-import * as piqiu3d from '@piqiu/piqiu3d'
-import { Piqiu3DRenderer } from './piQiuModule/Piqiu3DRenderer';
-import LoaderModel from '../../pages/LoaderModel';
+import React, { useEffect, useRef, useState } from "react";
+import * as piqiu3d from "@piqiu/piqiu3d";
+import { Piqiu3DRenderer } from "./piQiuModule/Piqiu3DRenderer";
+import LoaderModel from "../../pages/LoaderModel";
 
 export default function CanvasContainer() {
-    const canvasRef = useRef<HTMLCanvasElement>(null);
-    const sceneRef = useRef<any>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const sceneRef = useRef<any>(null);
 
-    useEffect(() => {
-        if (!canvasRef.current) return;
+  useEffect(() => {
+    if (!canvasRef.current) return;
 
-        const canvas = canvasRef.current;
-        canvas.width = window.innerWidth * 0.5;
-        canvas.height = window.innerHeight * 0.5;
+    const canvas = canvasRef.current;
+    canvas.width = window.innerWidth * 0.5;
+    canvas.height = window.innerHeight * 0.5;
 
-        const piqiuRenderer = new Piqiu3DRenderer(canvas, { width: canvas.width, height: canvas.height });
-        sceneRef.current = piqiuRenderer.scene;
+    const piqiuRenderer = new Piqiu3DRenderer(canvas, {
+      width: canvas.width,
+      height: canvas.height,
+    });
+    sceneRef.current = piqiuRenderer.scene;
 
-        debugger;
-        LoaderModel(piqiuRenderer);
-    }, []);
+    debugger;
+    LoaderModel(piqiuRenderer);
 
-    async function LoaderModel(piqiuRenderer: Piqiu3DRenderer) {
-        const loader = new piqiu3d.GLTFLoader();
-        debugger;
-        const meshes = await loader.load(piqiuRenderer.renderContext, '/models/teachers_desk.glb');
+    piqiuRenderer.updateCamera();
+  }, []);
 
-        for (const mesh of meshes) {
-            mesh.draw(piqiuRenderer.renderContext);
-        }
+  async function LoaderModel(piqiuRenderer: Piqiu3DRenderer) {
+    const loader = new piqiu3d.GLTFLoader();
+    debugger;
+    const meshes = await loader.load(
+      piqiuRenderer.renderContext,
+      //   "/models/teachers_desk.glb",
+      "/models/sample.gltf",
+    );
 
-        piqiuRenderer.addPart(loaderPart);
-
-        piqiuRenderer.addGeneralEventListener();
-
-        piqiuRenderer.updateCamera();
+    for (const mesh of meshes) {
+      piqiuRenderer.addPart(mesh);
     }
 
-    return (
-        <div>
-            <canvas
-                ref={canvasRef}
-                id="demo"
-            ></canvas>
-        </div>
-    );
+    piqiuRenderer.addGeneralEventListener();
+
+    piqiuRenderer.updateCamera();
+  }
+
+  return (
+    <div>
+      <canvas ref={canvasRef} id="demo"></canvas>
+    </div>
+  );
 }
